9a6e12eca46a43f5facba0bac44b029a6ca907bf
isidoromendez <isidoro.mendez@gmail.com>
2019-04-18 12:26:54 -0300

    api mail ok

---
+++ api/mail.php
@@ -0,0 +1,53 @@
+<?php
+require '../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+
+
+$cfg = json_decode(file_get_contents("cfg.json"));
+$request = json_decode(file_get_contents('php://input'));
+$res["err"] = "$request->cmd no callable.";
+
+if(is_callable($request->cmd)){
+  $res = call_user_func ($request->cmd , $request);
+}
+print json_encode($res);
+
+function send_contactmail($req){
+  global $cfg;
+  $cfgMail = $cfg->phpmailer;
+
+
+  $mail = new PHPMailer;
+  $mail->isSMTP();
+  $mail->Host = $cfgMail->host;
+  $mail->Port = $cfgMail->port;
+  $mail->SMTPAuth = $cfgMail->SMTPAuth;
+  $mail->Username = $cfgMail->username;
+  $mail->Password = $cfgMail->password;
+  $mail->setFrom('hola@snowfunclub.com.ar', 'SFC Contacto');
+  $mail->addAddress('hola@snowfunclub.com.ar', 'Escuela');
+  //if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+  if ($mail->addReplyTo($req->from, $req->name)) {
+      $mail->Subject = 'Contacto web - SFC';
+      $mail->isHTML(false);
+      $mail->Body = <<<EOT
+Email: {$req->from}
+Name: {$req->name}
+Message: {$req->message}
+EOT;
+  $res = array();
+          if (!$mail->send()) {
+              $res["err"] = true;
+              $res["msg"] = 'Sorry, something went wrong. Please try again later. ' . $mail->ErrorInfo;
+          } else {
+              $res["err"] = false;
+              $res["msg"] = 'Message sent! Thanks for contacting us.';
+          }
+      } else {
+          $res["err"] = true;
+          $res["msg"] = 'Invalid email address, message ignored.';
+      }
+
+  return $res;
+}
+ ?>
